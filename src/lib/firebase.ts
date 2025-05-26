26398d293a5a18d55a45615683975756dc3e82e1
kendrickweb94 <kendrickzgeorge@gmail.com>
2025-05-26 11:38:26 -0800

    fixed analytics

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword as signInWithEmail, signInAnonymously } from "firebase/auth";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword as signInWithEmail,
+  signInAnonymously,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -13,21 +20,23 @@ const firebaseConfig = {
   storageBucket: "daily-tasks-26df6.firebasestorage.app",
   messagingSenderId: "148816350600",
   appId: "1:148816350600:web:3b47afd8d4446648063e3d",
-  measurementId: "G-4HJZ7WFHMB"
+  measurementId: "G-4HJZ7WFHMB",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 //@-ts-ignore
-const analytics = getAnalytics(app);
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
 // Authentication functions
 export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
-export const signUpWithEmail = (email: string, password: string) => createUserWithEmailAndPassword(auth, email, password);
-export const signInWithEmailAndPassword = (email: string, password: string) => signInWithEmail(auth, email, password);
+export const signUpWithEmail = (email: string, password: string) =>
+  createUserWithEmailAndPassword(auth, email, password);
+export const signInWithEmailAndPassword = (email: string, password: string) =>
+  signInWithEmail(auth, email, password);
 export const signInAsAnonymous = () => signInAnonymously(auth);
 
 export { auth, db };
